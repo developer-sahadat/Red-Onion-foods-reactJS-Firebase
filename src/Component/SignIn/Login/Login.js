2fa4139e68a5d37756dec1729c3d49e9c10e8bd6
developer-sahadat <mdridoysikder862@gmail.com>
2022-04-16 03:13:52 +0600

    login and sign up design

--- src/Component/SignIn/Login/Login.js
+++ src/Component/SignIn/Login/Login.js
@@ -1,7 +1,26 @@
 import React from "react";
-
+import logo from "../../../Assets/images/logo2.png";
 const Login = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <div className="m-5">
+        <div className="container">
+          <img
+            style={{ width: "300px", margin: " 20px auto", display: "block" }}
+            src={logo}
+            alt=""
+          />
+          <form className="input-form">
+            <input type="email" placeholder="email"></input>
+            <br></br>
+            <input type="password" placeholder="password"></input>
+            <br></br>
+            <button className="btn-sign">Sign Up</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Component/SignIn/SignUp/SignUp.js
+++ src/Component/SignIn/SignUp/SignUp.js
@@ -1,7 +1,30 @@
 import React from "react";
+import "./SignUp.css";
+import logo from "../../../Assets/images/logo2.png";
 
 const SignUp = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="m-5">
+      <div className="container">
+        <img
+          style={{ width: "300px", margin: " 20px auto", display: "block" }}
+          src={logo}
+          alt=""
+        />
+        <form className="input-form">
+          <input type="text" placeholder="Name"></input>
+          <br></br>
+          <input type="email" placeholder="email"></input>
+          <br></br>
+          <input type="password" placeholder="password"></input>
+          <br></br>
+          <input type="password" placeholder="Confirm Password"></input>
+          <br></br>
+          <button className="btn-sign">Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
